16ab790ad658e0059469288420210317ccb48e0d
William Wang@mac <459226479@qq.com>
2022-10-03 21:28:43 +0800

    added javadoc comment

--- Simple.java
+++ Simple.java
@@ -1,5 +1,17 @@
-class Simple {
+/**
+ * @author William Wang
+ * a simple java class which prints lines to stdout
+ */
+public class Simple {
+    /** name of the Simple object */
+    public String name;
+    /**
+     * @param args arguments to pass to command line or applications
+     * @since 1.0
+     */
     public static void main(String[] args) {
         System.out.println("hey");
+        System.out.println("great");
+        System.out.println("nice");
     }
 }
